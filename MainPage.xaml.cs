42217956e58c483f34e2f47ad190795a5c2245ad
Daniel Ancines <daniel.ancines@gmail.com>
2024-10-20 17:11:58 -0300

    Certificate Pinning Tests & Certificate with IHttpClientFactory

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,16 +1,20 @@
 ﻿using Maui.DotObfuscatorCommons;
 using System.Collections.ObjectModel;
+using System.Security.Cryptography;
 
 namespace Maui.DotObfuscatorApp;
 
 public partial class MainPage : ContentPage
 {
     const string Url = "http://google.com";
+    private readonly IHttpClientFactory _httpClientFactory;
+
     public ObservableCollection<Person> People { get; } = new();
-    public MainPage()
+    public MainPage(IHttpClientFactory httpClientFactory)
     {
         InitializeComponent();
         this.Loaded += MainPage_Loaded;
+        this._httpClientFactory = httpClientFactory;
     }
 
     private void MainPage_Loaded(object? sender, EventArgs e)
@@ -18,14 +22,29 @@ public partial class MainPage : ContentPage
         this.PersonList.ItemsSource = People;
     }
 
-    private void OnCounterClicked(object sender, EventArgs e)
+    private async void OnCounterClicked(object sender, EventArgs e)
     {
+        string fileHash;
+        using (var md5 = MD5.Create())
+        {
+            using (var stream = File.OpenRead("D:\\code\\Maui.DotObfuscatorApp\\Obfuscar\\obfuscate.xml"))
+            {
+                var hash = md5.ComputeHash(stream);
+                fileHash = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+            }
+        }
+
+        var httpClient = this._httpClientFactory.CreateClient(HttpClientTypes.Regular);
+        await httpClient.GetAsync("https://efs.btgpactualdigital.com/trader");
+
         var person = new Person()
         {
-            Name = "Obfuscator App",
+            Name = fileHash,
             Age = Random.Shared.Next(0, 100)
         };
 
         this.People.Add(person);
     }
+
+
 }
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,23 +1,49 @@
-﻿namespace Maui.DotObfuscatorApp
+﻿using Microsoft.Extensions.Logging;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Maui.DotObfuscatorApp;
+
+public static class HttpClientTypes
 {
-    public static class MauiProgram
+    public readonly static string Regular = "Regular";
+}
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-            return builder.Build();
-        }
+        var certificateHandler = new HttpClientHandler();
+        certificateHandler.ClientCertificateOptions = ClientCertificateOption.Manual;
+        certificateHandler.ServerCertificateCustomValidationCallback = ValidateCertificate;
+
+        builder.Services.AddHttpClient();
+        builder.Services.AddHttpClient(HttpClientTypes.Regular, (client) =>
+        {
+            client.BaseAddress = new Uri("https://efs.btgpactual.com");
+        }).ConfigurePrimaryHttpMessageHandler(() => certificateHandler);
+
+        builder.Services.AddSingleton<MainPage>();
+        return builder.Build();
+    }
+
+    private static bool ValidateCertificate(HttpRequestMessage message, X509Certificate2? certificate, X509Chain? chain, SslPolicyErrors errors)
+    {
+        var certificateHash = certificate?.GetCertHashString();
+
+        return string.IsNullOrEmpty(certificateHash);
     }
 }
